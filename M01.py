6ef37fceb0720ce9e7a7cd22571d695f1f77926f
Administrator <RahulG1991>
2022-05-01 11:17:53 +0530

    Initial commit

---
+++ M01.py
@@ -0,0 +1,9 @@
+#THIS IS FOR MANUAL & GASOLINE CARS
+class Man_Gasoline:
+    colour=None
+    tank_cap=None
+    sun_roof=True
+
+    def details(self,*args):
+        print("You have entered below features : \n Colour :",self.colour,"\nTank Capacity : ",self.tank_cap,"\n Its Sunroof :",self.sun_roof)
+
